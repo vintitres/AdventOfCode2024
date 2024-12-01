241918ff713cee1ade312c1c10eb90f71183000b
jaqb <jaqb@sekta.pl>
2024-12-01 15:18:52 -0800

    d1p1

--- src/day01.rs
+++ src/day01.rs
@@ -1,13 +1,14 @@
-pub fn part1(input: &str) -> u32 {
-    input
+pub fn part1(input: &str) -> u64 {
+    let locs: Vec<Vec<u64>> = input
         .lines()
         .map(|l| {
-            let mut digits = l.chars().flat_map(|c| c.to_digit(10));
-            let f = digits.next().unwrap();
-            let l = digits.last().unwrap_or(f);
-            f * 10 + l
-        })
-        .sum()
+            l.split_whitespace().map(|n| n.parse::<u64>().unwrap()).collect()
+        }).collect();
+    let mut left: Vec<u64> = locs.iter().map(|l| l[0]).collect();
+    let mut right: Vec<u64> = locs.iter().map(|l| l[1]).collect();
+    left.sort();
+    right.sort();
+    left.iter().zip(right).map(|(l, r)| if *l > r { *l - r } else { r - *l }).sum()
 }
 
 pub fn part2(input: &str) -> u32 {
@@ -41,10 +42,9 @@ mod tests {
         include_str!("../input/2024/day1.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 55029);
+        assert_eq!(part1(input()), 1603498);
     }
 
     #[ignore = "not implemented"]
