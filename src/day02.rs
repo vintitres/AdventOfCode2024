d56eac39b589b34983af6e9c4748d478dcc31eaf
jaqb <jaqb@sekta.pl>
2024-12-01 22:15:13 -0800

    fmt

--- src/day02.rs
+++ src/day02.rs
@@ -6,39 +6,53 @@ pub fn part1(input: &str) -> usize {
                 .split_whitespace()
                 .map(|level| level.parse::<u64>().unwrap())
                 .collect();
-            let mut last: Option<u64> = None;
-            let mut dec = None;
-            for level in levels {
-                if last.is_some() {
-                    let last = last.unwrap();
-                    if dec.is_none() {
-                        dec = Some(last > level);
-                        if last == level {
-                            return false;
-                        }
-                    }
-                    let dec = dec.unwrap();
-                    if dec && last > level {
-                        if last - level > 3 {
-                            return false;
-                        }
-                    } else if !dec && last < level {
-                        if level - last > 3 {
-                            return false;
-                        }
-                    } else {
-                        return false;
-                    }
-                }
-
-                last = Some(level);
-            }
-            true
-        }).count()
+            check(&levels)
+        })
+        .count()
 }
 
-pub fn part2(input: &str) -> u64 {
-    input.lines().count() as u64
+fn check(levels: &Vec<u64>) -> bool {
+    let mut last: Option<u64> = None;
+    let mut dec = None;
+    for &level in levels {
+        if last.is_some() {
+            let last = last.unwrap();
+            if dec.is_none() {
+                dec = Some(last > level);
+                if last == level {
+                    return false;
+                }
+            }
+            let dec = dec.unwrap();
+            if dec && last > level {
+                if last - level > 3 {
+                    return false;
+                }
+            } else if !dec && last < level {
+                if level - last > 3 {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        last = Some(level);
+    }
+    true
+}
+
+pub fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|line| {
+            let levels: Vec<u64> = line
+                .split_whitespace()
+                .map(|level| level.parse::<u64>().unwrap())
+                .collect();
+            check(&levels)
+        })
+        .count()
 }
 
 #[cfg(test)]
