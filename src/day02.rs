f3cd6d697df56dd4fecf2d5f807bd208a55c0844
jaqb <jaqb@sekta.pl>
2024-12-01 22:22:09 -0800

    22

--- src/day02.rs
+++ src/day02.rs
@@ -6,51 +6,57 @@ pub fn part1(input: &str) -> usize {
                 .split_whitespace()
                 .map(|level| level.parse::<u64>().unwrap())
                 .collect();
-            check(&levels)
+            check(&levels).is_none()
         })
         .count()
 }
 
-fn check(levels: &Vec<u64>) -> bool {
+fn check(levels: &Vec<u64>) -> Option<usize> {
     let mut last: Option<u64> = None;
     let mut dec = None;
+    let mut i = 0;
     for &level in levels {
         if last.is_some() {
             let last = last.unwrap();
             if dec.is_none() {
                 dec = Some(last > level);
                 if last == level {
-                    return false;
+                    return Some(i);
                 }
             }
             let dec = dec.unwrap();
             if dec && last > level {
                 if last - level > 3 {
-                    return false;
+                    return Some(i);
                 }
             } else if !dec && last < level {
                 if level - last > 3 {
-                    return false;
+                    return Some(i);
                 }
             } else {
-                return false;
+                return Some(i);
             }
         }
-
+        i += 1;
         last = Some(level);
     }
-    true
+    None
 }
 
 pub fn part2(input: &str) -> usize {
     input
         .lines()
         .filter(|line| {
-            let levels: Vec<u64> = line
+            let mut levels: Vec<u64> = line
                 .split_whitespace()
                 .map(|level| level.parse::<u64>().unwrap())
                 .collect();
-            check(&levels)
+            if let Some(i) = check(&levels) {
+                levels.remove(i);
+                check(&levels).is_none()
+            } else {
+                true
+            }
         })
         .count()
 }
