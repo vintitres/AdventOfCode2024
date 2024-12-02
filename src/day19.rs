2987035908710d26ac89a19d918543376a02d9ad
jaqb <jaqb@sekta.pl>
2024-12-01 18:36:30 -0800

    future

---
+++ src/day19.rs
@@ -0,0 +1,28 @@
+pub fn part1(input: &str) -> u64 {
+    input.lines().count() as u64
+}
+
+pub fn part2(input: &str) -> u64 {
+    input.lines().count() as u64
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn input() -> &'static str {
+        include_str!("../input/2024/day19.txt")
+    }
+
+    #[ignore = "not implemented"]
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(input()), 1603498);
+    }
+
+    #[ignore = "not implemented"]
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(input()), 25574739);
+    }
+}
