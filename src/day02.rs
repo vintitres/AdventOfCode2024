63930a1831895b762f9c7824be3b616dc930565c
jaqb <jaqb@sekta.pl>
2024-12-01 21:51:20 -0800

    d2p1

--- src/day02.rs
+++ src/day02.rs
@@ -1,5 +1,45 @@
-pub fn part1(input: &str) -> u64 {
-    input.lines().count() as u64
+pub fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|line| {
+            let levels: Vec<u64> = line
+                .split_whitespace()
+                .map(|level| level.parse::<u64>().unwrap())
+                .collect();
+            let mut last: Option<u64> = None;
+            let mut dec = None;
+            for level in levels {
+                if last.is_some() {
+                    let last = last.unwrap();
+                    if dec.is_none() {
+                        dec = Some(last > level);
+                        if last == level {
+                            dbg!(last);
+                            return false;
+                        }
+                    }
+                    let dec = dec.unwrap();
+                    if dec && last > level {
+                        if last - level > 3 {
+                            dbg!(last);
+                            return false;
+                        }
+                    } else if !dec && last < level {
+                        if level - last > 3 {
+                            dbg!(last);
+                            return false;
+                        }
+                    } else {
+                        dbg!(last);
+                        return false;
+                    }
+                }
+
+                last = Some(level);
+            }
+            true
+        })
+        .count()
 }
 
 pub fn part2(input: &str) -> u64 {
@@ -14,10 +54,9 @@ mod tests {
         include_str!("../input/2024/day2.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 1603498);
+        assert_eq!(part1(input()), 287);
     }
 
     #[ignore = "not implemented"]
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let input = include_str!("../input/2024/day1.txt");
-    dbg!(adventofcode2024::day01::part2(input));
+    let input = include_str!("../input/2024/day2.txt");
+    dbg!(adventofcode2024::day02::part1(input));
 }
