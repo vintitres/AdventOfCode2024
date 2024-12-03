1923487051b292235bcdfe087b295cd82a2cde9b
jaqb <jaqb@sekta.pl>
2024-12-03 11:38:42 -0800

    cl

--- src/day03.rs
+++ src/day03.rs
@@ -1,39 +1,43 @@
+use std::str::Chars;
+
+pub fn read_num(chars: &mut Chars<'_>, last_index: &mut usize, expected_end: char) -> Option<u64> {
+    let mut n: u64 = 0;
+    for c in chars.by_ref() {
+        *last_index += 1;
+        if c.is_ascii_digit() {
+            n *= 10;
+            n += c.to_digit(10).unwrap() as u64;
+        } else if c == expected_end {
+            return Some(n);
+        } else {
+            return None;
+        }
+    }
+    None
+}
+
 pub fn part1(input: &str) -> u64 {
     let mut chars = input.chars();
     let mut last_index = 0;
-    let indices: Vec<usize> = input.match_indices("mul(").map(|(index, _)| index).collect();
-   indices.into_iter().map(|index|{
-        //chars.skip(index + 4 - last_index);
-        while last_index < index + 4 {
-            last_index += 1;
-            chars.next();
-        }
-        let mut n1: u64 = 0;
-        while let Some(c) = chars.next() {
-            last_index += 1;
-            if c.is_ascii_digit() {
-                n1 *= 10;
-                n1 += c.to_digit(10).unwrap() as u64;
-            } else if c == ',' {
-                break;
-            } else {
-                return 0;
+    let indices: Vec<usize> = input
+        .match_indices("mul(")
+        .map(|(index, _)| index)
+        .collect();
+    indices
+        .into_iter()
+        .map(|index| {
+            while last_index < index + 4 {
+                last_index += 1;
+                chars.next();
             }
-        }
-        let mut n2 = 0;
-        while let Some(c) = chars.next() {
-            last_index += 1;
-            if c.is_ascii_digit() {
-                n2 *= 10;
-                n2 += c.to_digit(10).unwrap() as u64;
-            } else if c == ')' {
-                return n1 * n2;
-            } else {
-                return 0;
+            if let Some(n1) = read_num(&mut chars, &mut last_index, ',') {
+                if let Some(n2) = read_num(&mut chars, &mut last_index, ')') {
+                    return n1 * n2;
+                }
             }
-        }
-        0
-    }).sum()
+            0
+        })
+        .sum()
 }
 
 pub fn part2(input: &str) -> u64 {
