bf8ae66709f5855f5d8a727f80b0ee8267350ce8
jaqb <jaqb@sekta.pl>
2024-12-05 14:48:29 -0700

    5acpp

--- src/cpp/day05.cpp
+++ src/cpp/day05.cpp
@@ -1,23 +1,57 @@
 #include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+
 using namespace std;
 
+int check(string const& line, unordered_map<int, unordered_set<int>> const& rules) {
+    unordered_set<int> seen;
+    stringstream ss(line);
+    vector<int> nums;
+    string ns;
+    while (getline(ss, ns, ',')) {
+        int n;
+        stringstream nss(ns);
+        nss >> n;
+        nums.push_back(n);
+        seen.insert(n);
+    }
+    for (int n : nums) {
+        seen.erase(n);
+        auto rule_it = rules.find(n);
+        if (rule_it != rules.end()) {
+            for (int req : rule_it->second) {
+                if (seen.find(req) != seen.end()) {
+                    return 0;
+                }
+            }
+        }
+    }
+    return nums[nums.size() / 2];
+}
+
 int main() {
-    int l, r;
-    vector<int> left, right;
-    while(cin >> l >> r) {
-        left.push_back(l);
-        right.push_back(r);
+    string line = "s";
+    unordered_map<int, unordered_set<int>> rules;
+    while (line != "") {
+        getline(cin, line);
+        std::stringstream ss;
+        ss << line;
+        int n1, n2;
+        char c;
+        ss >> n1 >> c >> n2;
+        auto it = rules.find(n2);
+        if (it == rules.end()) {
+            rules[n2] = unordered_set<int>();
+            it = rules.find(n2);
+        }
+        (*it).second.insert(n1);
     }
-    sort(left.begin(), left.end());
-    sort(right.begin(), right.end());
-    auto left_it = left.begin();
-    auto right_it = right.begin();
-    int s = 0;
-    for (; left_it != left.end();) {
-        s += abs(*left_it - *right_it);
-        ++right_it;
-        ++left_it;
+    int sum = 0;
+    while (cin >> line) {
+        sum += check(line, rules);
     }
-    cout << s << endl;
+    cout << sum << endl;
     return 0;
 }
