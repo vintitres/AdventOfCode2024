fea8b3bfa61633cd8a26e94de241fd5e66e70cf6
jaqb <jaqb@sekta.pl>
2024-12-05 17:08:24 -0700

    5arust

--- src/day05.rs
+++ src/day05.rs
@@ -1,5 +1,60 @@
+use std::collections::{HashMap, HashSet};
+
+use itertools::Itertools;
+
+type Rules = HashMap<u64, HashSet<u64>>;
+
+fn read_rules(input: &str) -> Rules {
+    let mut rules = Rules::new();
+    input
+        .lines()
+        .take_while(|l| !l.is_empty())
+        .map(|l| {
+            l.split('|')
+                .map(|num| num.parse::<u64>().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
+        .for_each(|(n1, n2)| {
+            rules.entry(n2).or_insert(HashSet::<u64>::new()).insert(n1);
+        });
+    rules
+}
+
+fn read_update(line: &str) -> Vec<u64> {
+    line.split(',').map(|n| n.parse::<u64>().unwrap()).collect()
+}
+
 pub fn part1(input: &str) -> u64 {
-    input.lines().count() as u64
+    let rules = read_rules(input);
+    input
+        .lines()
+        .skip_while(|l| !l.is_empty())
+        .skip(1)
+        .map(|l| {
+            let update = read_update(l);
+            let mut all = HashSet::<u64>::from_iter(update.iter().cloned());
+            let mid_i = update.len() / 2;
+            let mut mid = 0;
+            for (i, n) in update.iter().enumerate() {
+                if i == mid_i {
+                    mid = n.clone();
+                }
+                match rules.get(&n) {
+                    Some(deps) => {
+                        for dep in deps {
+                            if all.contains(&dep) {
+                                return 0;
+                            }
+                        }
+                    }
+                    None => {}
+                }
+                all.remove(&n);
+            }
+            mid
+        })
+        .sum()
 }
 
 pub fn part2(input: &str) -> u64 {
@@ -14,7 +69,6 @@ mod tests {
         include_str!("../input/2024/day5.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
         assert_eq!(part1(input()), 7365);
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use adventofcode2024::day04 as day;
+use adventofcode2024::day05 as day;
 
 fn main() {
-    let input = include_str!("../input/2024/day4.txt");
+    let input = include_str!("../input/2024/day5.txt");
     dbg!(day::part1(input));
     dbg!(day::part2(input));
 }
