b7a6d997ff44b6a2f75845264b709b9a9f1904f5
jaqb <jaqb@sekta.pl>
2024-12-08 11:30:32 -0700

    8unify

--- src/day08.rs
+++ src/day08.rs
@@ -30,38 +30,47 @@ impl World {
         antennas
     }
 
-    fn antinodes(&self, a1: &Pos, a2: &Pos) -> Vec<Pos> {
-        let mut v = vec![];
-        if *a1 != *a2 {
-            let diffx = a1.0 - a2.0;
-            let diffy = a1.1 - a2.1;
-            let mut add = |x: isize, y: isize| {
-                if x >= 0 && x < self.height() as isize && y >= 0 && y < self.width() as isize {
-                    v.push((x, y));
+    fn find_antinodes(&self, limited: bool) -> HashSet<Pos> {
+        let antennas = self.find_antennas();
+        let mut antinodes = HashSet::<Pos>::new();
+        for (_freq, antennas) in antennas.iter() {
+            for antenna1 in antennas {
+                for antenna2 in antennas {
+                    antinodes.extend(self.antinodes(antenna1, antenna2, limited).iter());
                 }
-            };
-            add(a1.0 + diffx, a1.1 + diffy);
-            add(a2.0 - diffx, a2.1 - diffy);
+            }
         }
-        v
+        antinodes
     }
 
-    fn antinodes2(&self, a1: &Pos, a2: &Pos) -> Vec<Pos> {
+    fn antinodes(&self, a1: &Pos, a2: &Pos, limited: bool) -> Vec<Pos> {
         let mut v = vec![];
         if *a1 != *a2 {
             let diffx = a1.0 - a2.0;
             let diffy = a1.1 - a2.1;
-            let mut x = a1.0;
-            let mut y = a1.1;
-            while x >= 0 && x < self.height() as isize && y >= 0 && y < self.width() as isize {
-                v.push((x, y));
-                x += diffx;
-                y += diffy;
-            }
+            let mut x = a2.0 - diffx;
+            let mut y = a2.1 - diffy;
             while x >= 0 && x < self.height() as isize && y >= 0 && y < self.width() as isize {
                 v.push((x, y));
                 x -= diffx;
                 y -= diffy;
+                if limited {
+                    break;
+                }
+            }
+            let mut x = a1.0 + diffx;
+            let mut y = a1.1 + diffy;
+            while x >= 0 && x < self.height() as isize && y >= 0 && y < self.width() as isize {
+                v.push((x, y));
+                x += diffx;
+                y += diffy;
+                if limited {
+                    break;
+                }
+            }
+            if !limited {
+                v.push(*a1);
+                v.push(*a2);
             }
         }
         v
@@ -79,31 +88,11 @@ impl World {
 type Pos = (isize, isize);
 
 pub fn part1(input: &str) -> usize {
-    let world = World::read(input);
-    let antennas = world.find_antennas();
-    let mut antinodes = HashSet::<Pos>::new();
-    for (_freq, antennas) in antennas.iter() {
-        for antenna1 in antennas {
-            for antenna2 in antennas {
-                antinodes.extend(world.antinodes(antenna1, antenna2).iter());
-            }
-        }
-    }
-    antinodes.len()
+    World::read(input).find_antinodes(true).len()
 }
 
 pub fn part2(input: &str) -> usize {
-    let world = World::read(input);
-    let antennas = world.find_antennas();
-    let mut antinodes = HashSet::<Pos>::new();
-    for (_freq, antennas) in antennas.iter() {
-        for antenna1 in antennas {
-            for antenna2 in antennas {
-                antinodes.extend(world.antinodes2(antenna1, antenna2).iter());
-            }
-        }
-    }
-    antinodes.len()
+    World::read(input).find_antinodes(false).len()
 }
 
 #[cfg(test)]
