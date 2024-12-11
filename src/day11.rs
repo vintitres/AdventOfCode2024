c936131bbb5f2cb4a5b828ff6c05f06bffe974df
jaqb <jaqb@sekta.pl>
2024-12-11 13:54:11 -0700

    11

--- src/day11.rs
+++ src/day11.rs
@@ -1,9 +1,55 @@
-pub fn part1(input: &str) -> u64 {
-    input.lines().count() as u64
+use std::collections::HashMap;
+
+type Mem = HashMap<(u64, usize), usize>;
+
+fn half(n: u64) -> Option<(u64, u64)> {
+    let n = n.to_string();
+    if n.len() % 2 == 0 {
+        let (l, r) = n.split_at(n.len() / 2);
+        Some((l.parse::<u64>().unwrap(), r.parse::<u64>().unwrap()))
+    } else {
+        None
+    }
 }
 
-pub fn part2(input: &str) -> u64 {
-    input.lines().count() as u64
+fn stones(num: u64, steps: usize, mem: &mut Mem) -> usize {
+    if steps == 0 {
+        return 1;
+    }
+    let key = (num, steps);
+    if let Some(s) = mem.get(&key) {
+        *s
+    } else {
+        let s = if num == 0 {
+            stones(1, steps - 1, mem)
+        } else if let Some((l, r)) = half(num) {
+            stones(l, steps - 1, mem) + stones(r, steps - 1, mem)
+        } else {
+            stones(num * 2024, steps - 1, mem)
+        };
+        mem.insert(key, s);
+        s
+    }
+}
+
+pub fn part1(input: &str) -> usize {
+    let mut mem = Mem::new();
+    input
+        .trim()
+        .split(' ')
+        .map(|word| word.parse::<u64>().unwrap())
+        .map(|num| stones(num, 25, &mut mem))
+        .sum()
+}
+
+pub fn part2(input: &str) -> usize {
+    let mut mem = Mem::new();
+    input
+        .trim()
+        .split(' ')
+        .map(|word| word.parse::<u64>().unwrap())
+        .map(|num| stones(num, 75, &mut mem))
+        .sum()
 }
 
 #[cfg(test)]
@@ -14,15 +60,13 @@ mod tests {
         include_str!("../input/2024/day11.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 1603498);
+        assert_eq!(part1(input()), 217443);
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 25574739);
+        assert_eq!(part2(input()), 257246536026785);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use adventofcode2024::day10 as day;
+use adventofcode2024::day11 as day;
 
 fn main() {
-    let input = include_str!("../input/2024/day10.txt");
+    let input = include_str!("../input/2024/day11.txt");
     dbg!(day::part1(input));
     dbg!(day::part2(input));
 }
