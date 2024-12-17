204efd48c5a5b5a8a63356b9833d70576d46ce8e
jaqb <jaqb@sekta.pl>
2024-12-16 17:49:12 -0700

    16prep

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use adventofcode2024::day15 as day;
+use adventofcode2024::day16 as day;
 
 fn main() {
-    let input = include_str!("../input/2024/day15.txt");
+    let input = include_str!("../input/2024/day16.txt");
     dbg!(day::part1(input));
     dbg!(day::part2(input));
 }
