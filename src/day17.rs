48ded7b86b34a5936337c607089f92ff360d0197
jaqb <jaqb@sekta.pl>
2024-12-17 17:26:12 -0700

    17b

--- src/day17.rs
+++ src/day17.rs
@@ -17,11 +17,13 @@ fn read(input: &str) -> (u32, u32, u32, Vec<u32>) {
     )
 }
 
-pub fn part1(input: &str) -> String {
-    let (mut a, mut b, mut c, instr) = read(input);
+fn run(a: u64, b: u32, c: u32, instr: &[u32]) -> Vec<u32> {
+    let mut a = a;
+    let mut b = b;
+    let mut c = c;
     let combo = |operand, a, b, c| match operand {
         0..=3 => operand,
-        4 => a,
+        4 => (a % 8) as u32,
         5 => b,
         6 => c,
         _ => unreachable!("unsupported combo operand: {}", operand),
@@ -35,45 +37,86 @@ pub fn part1(input: &str) -> String {
         .collect_vec();
     let mut output = Vec::new();
     while instr_i < instr.len() {
-        // TODO movable loopp
         let (opcode, &operand) = instr[instr_i];
         instr_i += 1;
         match opcode {
             0 => {
-                a = a / 2_u32.pow(combo(operand, a, b, c));
+                a = a / 2_u64.pow(combo(operand, a, b, c));
+                // dbg!("adv", operand, combo(operand, a, b, c));
             }
             1 => {
                 b ^= operand;
+                // dbg!("bxl", operand);
             }
             2 => {
                 b = combo(operand, a, b, c) % 8;
+                // dbg!("bst", operand, combo(operand, a, b, c));
             }
             3 => {
                 if a != 0 {
                     assert!(operand % 2 == 0);
                     instr_i = (operand / 2) as usize;
                 }
+                // dbg!("jnz", operand / 2);
             }
             4 => {
                 b ^= c;
+                // dbg!("bxc");
             }
             5 => {
-                output.push(combo(operand, a, b, c) % 8);
+                let o = combo(operand, a, b, c) % 8;
+                output.push(o);
+                // dbg!("out", operand, combo(operand, a, b, c) % 8);
             }
             6 => {
-                b = a / 2_u32.pow(combo(operand, a, b, c));
+                b = (a / 2_u64.pow(combo(operand, a, b, c)) % 8) as u32;
+                // dbg!("bdv", operand, combo(operand, a, b, c));
             }
             7 => {
-                c = a / 2_u32.pow(combo(operand, a, b, c));
+                c = (a / 2_u64.pow(combo(operand, a, b, c)) % 8) as u32;
+                // dbg!("cdv", operand, combo(operand, a, b, c));
             }
             _ => unreachable!("unsuppoerted opcode: {:?}", opcode),
         }
     }
-    output.iter().map(|n| n.to_string()).join(",")
+    output
 }
 
-pub fn part2(input: &str) -> u32 {
-    input.lines().count() as u32
+fn run_translated(mut a: u64) -> Vec<u32> {
+    let mut out = Vec::new();
+    while a != 0 {
+        let mut b = (a % 8) as u32;
+        b ^= 2;
+        let c = ((a / (2_u64.pow(b))) % 8) as u32;
+        a /= 2_u64.pow(3);
+        b ^= c;
+        b ^= 7;
+        out.push(b % 8);
+    }
+    out
+}
+
+pub fn part1(input: &str) -> String {
+    let (a, b, c, instr) = read(input);
+    run(a as u64, b, c, &instr)
+        .iter()
+        .map(|n| n.to_string())
+        .join(",")
+}
+
+pub fn part2(input: &str) -> u64 {
+    let (_a, _b, _c, instr) = read(input);
+    let mut a = 0_u64;
+    for i in instr.iter().rev() {
+        a *= 8;
+        for sub_a in 0..8 {
+            if run_translated(a + sub_a).first() == Some(i) {
+                a += sub_a;
+                break;
+            }
+        }
+    }
+    a
 }
 
 #[cfg(test)]
@@ -89,9 +132,8 @@ mod tests {
         assert_eq!(part1(input()), "7,1,3,7,5,1,0,3,4".to_string());
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 25574739);
+        assert_eq!(part2(input()), 190384113204239);
     }
 }
