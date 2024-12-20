53931aaf7664442e4ef976ea7bd6513128a9cb66
jaqb <jaqb@sekta.pl>
2024-12-20 12:13:19 -0700

    19a

--- src/day19.rs
+++ src/day19.rs
@@ -1,5 +1,32 @@
-pub fn part1(input: &str) -> u64 {
-    input.lines().count() as u64
+use std::collections::HashMap;
+
+fn is_possibe(design: &str, towels: &[&str], start: usize, seen: &mut HashMap<usize, bool>) -> bool {
+    // dbg!(design, towels, start);
+    if start == design.len() {
+        return true;
+    }
+    if seen.contains_key(&start) {
+        return *seen.get(&start).unwrap();
+    }
+    for towel in towels {
+        if design.split_at(start).1.starts_with(towel) {
+            if is_possibe(design, towels, start + towel.len(), seen) {
+                seen.insert(start, true);
+                return true;
+            }
+        }
+    }
+    seen.insert(start, false);
+    false
+}
+
+fn read_towels(input: &str) -> Vec<&str> {
+    input.lines().take(1).next().unwrap().split(", ").collect()
+}
+
+pub fn part1(input: &str) -> usize {
+    let towels = read_towels(input);
+    input.lines().skip(2).filter(|design| is_possibe(design, &towels, 0, &mut HashMap::<usize, bool>::new())).count()
 }
 
 pub fn part2(input: &str) -> u64 {
@@ -14,10 +41,9 @@ mod tests {
         include_str!("../input/2024/day19.txt")
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 1603498);
+        assert_eq!(part1(input()), 308);
     }
 
     #[ignore = "not implemented"]
