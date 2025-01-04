72e5581d16b48a8cf044580a4f96c3e9cebb2069
jaqb <jaqb@sekta.pl>
2025-01-04 15:17:26 +0100

    21wip

--- src/day21.rs
+++ src/day21.rs
@@ -1,5 +1,89 @@
+use std::io::{repeat, Read};
+
+use rayon::iter::once;
+
+enum Numpad {
+    Accept,
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+}
+
+enum Keypad {
+    Accept,
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn numeric(code: &str) -> u64 {
+    code.chars()
+        .flat_map(|c| c.to_digit(10))
+        .reduce(|n, d| n * 10 + d)
+        .unwrap() as u64
+}
+
+fn numpad_pos(c: char) -> (usize, usize) {
+    match c {
+        '7' => (0, 0),
+        '8' => (0, 1),
+        '9' => (0, 2),
+        '4' => (1, 0),
+        '5' => (1, 1),
+        '6' => (1, 2),
+        '1' => (2, 0),
+        '2' => (2, 1),
+        '3' => (2, 2),
+        '0' => (3, 1),
+        'A' => (3, 2),
+    }
+})
+
+fn type_code(code: &str) -> String {
+    let mut seq = String::new();
+    let mut state = 'A';
+    for c in code.chars() {
+        let (state_x, state_y) = numpad_pos(state);
+        let (c_x, c_y) = numpad_pos(c);
+        match state_x.cmp(&c_x) {
+            std::cmp::Ordering::Equal => String::new(),
+            std::cmp::Ordering::Greater => String::from_iter(repeat('^').take(state_x - c_x)),
+            std::cmp::Ordering::Less => String::from_iter(repeat('v').take(c_x - state_x)),
+
+        }
+        match state_y.cmp(&c_y) {
+            std::cmp::Ordering::Equal => String::new(),
+            std::cmp::Ordering::Greater => String::from_iter(repeat('<').take(state_y - c_y)),
+            std::cmp::Ordering::Less => String::from_iter(repeat('>').take(c_y - state_y)),
+
+        }
+        seq += "A";
+    }
+    seq
+}
+
+fn type_arrows(arrows: String) -> String {
+    let mut seq = String::new();
+    seq
+}
+
+fn shortest(code: &str) -> u64 {
+    type_arrows(type_arrows(type_code(code))).len() as u64
+}
+
 pub fn part1(input: &str) -> u64 {
-    input.lines().count() as u64
+    input
+        .lines()
+        .map(|line| numeric(line) * shortest(line))
+        .sum()
 }
 
 pub fn part2(input: &str) -> u64 {
