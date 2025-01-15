080a9c38387d6c10c5492adacf8803c604625d02
jaqb <jaqb@sekta.pl>
2025-01-15 13:29:55 +0000

    Revert mem
    This reverts commit b21d411ceec507f77004d201f7e00036807f1cb8.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use adventofcode2024::day22 as day;
 
 fn main() {
-    let input = include_str!("../input/2024/day22.txt");
+    let input = include_str!("../input/2024/day22e.txt");
     dbg!(day::part1(input));
     dbg!(day::part2(input));
 }
