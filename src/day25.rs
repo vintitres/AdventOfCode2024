9e705da8d49b6a5c1656694f8c366255be299873
jaqb <jaqb@sekta.pl>
2025-01-15 18:39:26 +0000

    25wip

--- src/day25.rs
+++ src/day25.rs
@@ -1,5 +1,80 @@
+use itertools::{Either, Itertools};
+
+fn parse(s: &str, cc: char) -> Vec<usize> {
+    let mut ret = vec![0,0,0,0,0];
+    for (i, line) in s.lines().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            if c == cc {
+                ret[j] = i;
+            }
+        }
+    }
+    ret
+
+}
+
+struct Key {
+    pins: Vec<usize>
+}
+
+impl Key {
+    fn parse(s: &str) -> Key {
+        Key {
+            pins: parse(s, '.')
+        }
+    }
+}
+
+struct Lock {
+    holes: Vec<usize>
+}
+impl Lock {
+    fn parse(s: &str) -> Lock {
+        Lock {
+            holes: parse(s, '#')
+        }
+    }
+
+    fn fit(&self, key: &Key) -> bool {
+        for (i, hole) in self.holes.iter().enumerate() {
+            if hole < key.pins.get(i).unwrap() {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+enum Schema {
+    Key(Key),
+    Lock(Lock),
+}
+
+impl Schema {
+    fn parse(s: &str) -> Schema {
+        if s.starts_with("#") {
+            Schema::Lock(Lock::parse(s))
+        } else {
+            Schema::Key(Key::parse(s))
+        }
+    }
+}
+
 pub fn part1(input: &str) -> u64 {
-    input.lines().count() as u64
+    let (keys, locks): (Vec<Key>, Vec<Lock>) = input.trim().split("\n\n").map(Schema::parse).partition_map(|s| match s {
+        Schema::Key(key) => Either::Left(key),
+        Schema::Lock(lock) => Either::Right(lock),
+    });
+    let mut matches = 0;
+    for key in keys {
+        for lock in &locks {
+            if lock.fit(&key) {
+                matches += 1;
+            }
+
+        }
+    }
+    matches
 }
 
 pub fn part2(input: &str) -> u64 {
