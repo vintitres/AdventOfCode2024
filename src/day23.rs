d0ec8e15814b66631fa9bd6bf81568bef10f3f7a
jaqb <jaqb@sekta.pl>
2025-01-15 14:10:09 +0000

    23?

--- src/day23.rs
+++ src/day23.rs
@@ -1,5 +1,80 @@
-pub fn part1(input: &str) -> u64 {
-    input.lines().count() as u64
+use std::collections::{HashMap, HashSet};
+
+use itertools::Itertools;
+
+const COMP_NAME_START: char = 't';
+
+fn char_letter_hash(c: char) -> u64 {
+    match c {
+        'a' => 0,
+        'b' => 1,
+        'c' => 2,
+        'd' => 3,
+        'e' => 4,
+        'f' => 5,
+        'g' => 6,
+        'h' => 7,
+        'i' => 8,
+        'j' => 9,
+        'k' => 10,
+        'l' => 11,
+        'm' => 12,
+        'n' => 13,
+        'o' => 14,
+        'p' => 15,
+        'q' => 16,
+        'r' => 17,
+        's' => 18,
+        't' => 19,
+        'u' => 20,
+        'v' => 21,
+        'w' => 22,
+        'x' => 23,
+        'y' => 24,
+        'z' => 25,
+        _ => unimplemented!(),
+    }
+}
+
+fn computer_hash(name: &str) -> u64 {
+    name.chars()
+        .map(char_letter_hash)
+        .reduce(|n, d| n * 100 + d)
+        .unwrap()
+}
+
+fn read_net(input: &str) -> HashMap<u64, HashSet<u64>> {
+    let mut ret = HashMap::new();
+    input.lines().for_each(|line| {
+        let (l, r) = line.split('-').map(computer_hash).collect_tuple().unwrap();
+        ret.entry(l).or_insert(HashSet::new()).insert(r);
+        ret.entry(r).or_insert(HashSet::new()).insert(l);
+    });
+    ret
+}
+
+pub fn part1(input: &str) -> usize {
+    let mut cnt = 0;
+    let net = read_net(input);
+    for &comp1 in net.keys() {
+        if comp1 / 100 != char_letter_hash(COMP_NAME_START) {
+            continue;
+        }
+        for &comp2 in net.get(&comp1).unwrap() {
+            if comp1 <= comp2 {
+                continue;
+            }
+            for &comp3 in net.get(&comp2).unwrap() {
+                if comp2 <= comp3 {
+                    continue;
+                }
+                if net.get(&comp3).unwrap().contains(&comp1) {
+                    cnt += 1;
+                }
+            }
+        }
+    }
+    cnt
 }
 
 pub fn part2(input: &str) -> u64 {
@@ -17,7 +92,7 @@ mod tests {
     #[ignore = "not implemented"]
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 1603498);
+        assert_eq!(part1(input()), 752); // ?
     }
 
     #[ignore = "not implemented"]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use adventofcode2024::day22 as day;
+use adventofcode2024::day23 as day;
 
 fn main() {
-    let input = include_str!("../input/2024/day22e.txt");
+    let input = include_str!("../input/2024/day23.txt");
     dbg!(day::part1(input));
     dbg!(day::part2(input));
 }
